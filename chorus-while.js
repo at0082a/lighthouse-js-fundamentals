0ef30a79e65cf0c3e834151f800add05d5e650ec
Alex Tilatti <alex.tilatti@gmail.com>
2019-02-11 19:21:12 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,22 @@
+/*var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+
+  if (repeat === 5) {
+   console.log("*change key*")
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+*/
+
+var chorus = "Let's Dance!"
+
+for (var i = 0; i < 10; i++) {
+
+  console.log(chorus);
+
+}
+
+console.log("Until the sun comes up!");
---
+++ packing-list.js
@@ -0,0 +1,7 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+for (i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+console.log(packingList[4]);
